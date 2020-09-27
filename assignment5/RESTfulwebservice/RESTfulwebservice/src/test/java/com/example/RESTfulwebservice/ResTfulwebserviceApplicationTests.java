ff9de90abd0049f889519f2985ff96acd9130bbf
crissb3 <crissb1337@hotmail.com>
2020-09-27 18:52:44 +0200

    First commit

---
+++ assignment5/RESTfulwebservice/RESTfulwebservice/src/test/java/com/example/RESTfulwebservice/ResTfulwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
